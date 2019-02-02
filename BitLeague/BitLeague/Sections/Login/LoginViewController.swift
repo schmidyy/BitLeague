be370b9f613902e6a956043e2a668409afbc34db
Mat Schmid <mat.schmid@shopify.com>
2019-02-01 23:35:23 -0500

    Added Login Foundations

---
+++ BitLeague/BitLeague/Sections/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  BitLeague
+//
+//  Created by Mat Schmid on 2019-02-01.
+//  Copyright © 2019 kirkbyo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
