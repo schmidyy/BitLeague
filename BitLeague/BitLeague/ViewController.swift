a2d298f99ec83f4807bc2f65bac09924e29d3793
kirkbyo <ozzie.kirkby@gmail.com>
2019-02-01 23:07:09 -0500

    Initial Commit

---
+++ BitLeague/BitLeague/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitLeague
+//
+//  Created by Ozzie Kirkby on 2019-02-01.
+//  Copyright © 2019 kirkbyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
